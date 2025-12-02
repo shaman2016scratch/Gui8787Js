474df1e069b5bd4dc1cba94f3b88f95e4e15f8a9
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-02 23:14:26 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -8,7 +8,7 @@ Gui8787Js = {
     "uninstall_Gui8787Js": function() { Gui8787JsConfig.installed = false }
   },
   "gui": {
-    "SetApp": function(a) { Gui8787JsConfig.app = a },
+    "setApp": function(a) { Gui8787JsConfig.app = a },
     "app": {
       "content": {
         "set": function(c) {
@@ -18,6 +18,34 @@ Gui8787Js = {
               "content": []
             }
             VrApp.content = c.split('; ')
+            for(i = 0; i < VrApp.content.length; i++) {
+              i2 = VrApp.content[i]
+              if (i2.name === "gui") {
+                for(i3 = 0; i3 < i2.content.length; i3++) {
+                  i4 = i2.content[i3]
+                  if (i4.name === 'app') {
+                    for(i5 = 0; i5 < i4.content.length; i5++) {
+                      i6 = i4.content[i5]
+                      i7 = i6.name
+                      i8 = i6.content
+                      i9 = i6.args
+                      if (i7 === 'jsCode') {
+                        html = `${html}<script>${i8}</script>`
+                      } else if (i7 === 'if') {
+                        if (i9.bool) {
+                          appp = Gui8787JsConfig.app
+                          Gui8787Js.gui.setApp(i9.id)
+                          Gui8787Js.gui.app.content.set(i8)
+                          Gui8787Js.gui.setApp = appp
+                        }
+                      } else if (i7 === 'html') {
+                        html = `${html}${i8}`
+                      }
+                    }
+                  }
+                }
+              }
+            }
           }
         }
       }
