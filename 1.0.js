cc0d9b185e21c25599cebabca9a6350d0e7aebff
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-02 21:35:56 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -1,1 +1,20 @@
-Gui8787Js = {}
+Gui8787JsConfig = {
+  "installed": false,
+  "app": 'root'
+}
+Gui8787Js = {
+  "config": {
+    "install_Gui8787Js": function() { Gui8787JsConfig.installed = true },
+    "uninstall_Gui8787Js": function() { Gui8787JsConfig.installed = false }
+  },
+  "gui": {
+    "SetApp": function(a) { Gui8787JsConfig.app = a },
+    "app": {
+      "content": {
+        "set": function(c) {
+          app = document.getElementById(Gui8787JsConfig.app)
+        }
+      }
+    }
+  }
+}
